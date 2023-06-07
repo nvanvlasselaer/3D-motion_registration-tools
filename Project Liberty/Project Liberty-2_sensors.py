1ec67f44057cda20bf6a83e987f3d6a2ba1243ed
ExanVUB <127668600+ExanVUB@users.noreply.github.com>
2023-06-08 01:32:36 +0200

    Added only calc-plot every 10 iterantions

--- Project Liberty/Project Liberty-2_sensors.py
+++ Project Liberty/Project Liberty-2_sensors.py
@@ -87,6 +87,8 @@ def read_fifo_data():
     distort1 = None
     distort2 = None
 
+    counter = 0
+
     buffer = ""
 
     while True:
@@ -119,9 +121,12 @@ def read_fifo_data():
                     distort2 = station2["distortion"]
 
                 if quat1 is not None and quat2 is not None:
-                    euler_angles = calculate_angular_difference(quat1, quat2)
-                    angles.append(euler_angles)
+                    # Calculate and store angular differences every 10 iterations
+                    if counter % 10 == 0:
+                        euler_angles = calculate_angular_difference(quat1, quat2)
+                        angles.append(euler_angles)
 
+                    counter += 1
                     calibration_text = f"sensor1={distort1} sensor2={distort2}"
                     text_box.delete(1.0, tk.END)
                     text_box.insert(tk.END, calibration_text)
